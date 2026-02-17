4d02ff7987395d5b44ac2deb45debd7fd2d57640
RadostinTasev1999 <radostintasev22@yahoo.com>
2026-02-17 09:06:32 +0200

    Changed host endpoint url

--- src/api/data.js
+++ src/api/data.js
@@ -1,7 +1,7 @@
 import * as api from './api.js';
 
-const host = 'http://localhost:3030'
-api.settings.host = 'http://localhost:3030';
+const host = 'https://server-library-catalog.onrender.com'
+api.settings.host = 'https://server-library-catalog.onrender.com';
 
 export const login = api.login;
 export const register = api.register;
